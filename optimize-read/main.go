ec3145c87b9afba043cb961378d38dfd280101d5
Mohit <kumarmohit2496@gmail.com>
2024-11-01 13:11:50 +0530

    file reader now reads line by line

--- optimize-read/main.go
+++ optimize-read/main.go
@@ -57,14 +57,57 @@ func readScannerBytes(f *os.File) {
 }
 
 func readByFileReader(f *os.File) {
+	// pos, neg := 0, 0
 	buffer := make([]byte, 2048*2048)
+	leftover := []byte{}
 	for {
-		_, err := f.Read(buffer)
+		bytesRead, err := f.Read(buffer)
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			panic(err)
+		}
+
+		// Combine leftover with current buffer
+		chunk := append(leftover, buffer[:bytesRead]...)
+
+		lastNewLine := -1
+		for i := len(chunk) - 1; i >= 0; i-- {
+			if chunk[i] == 10 { //10 is the ASCII value of newline
+				lastNewLine = i
+				break
+			}
+		}
+
+		var processUntil int
+		// No newline found, entire chunk becomes leftover
+		if lastNewLine == -1 {
+			leftover = chunk
+			continue
+		} else {
+			processUntil = lastNewLine + 1
+			leftover = chunk[processUntil:]
+		}
+
+		byteLines := chunk[:processUntil]
+
+		bufIndex := 0
+		for {
+			// Extracting a single line from the multiple lines inside the byteLines slice
+			var start, end int
+			for i := bufIndex; i < len(byteLines); i++ {
+				if byteLines[i] == 10 {
+					start, end = bufIndex, i
+					bufIndex = i + 1
+					break
+				}
+			}
+
+			if bufIndex == len(byteLines) {
+				break
+			}
+			_ = byteLines[start:end]
 		}
 	}
 }
